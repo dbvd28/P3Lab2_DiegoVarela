244fd81cac3341bdaba24228a06bccea87ab499f
Diego Varela <diegocrash124@gmail.com>
2020-07-31 16:29:34 -0600

    Se crea el ejercicio 1

--- p3Lab2_DiegoVarelaa1.cpp
+++ p3Lab2_DiegoVarelaa1.cpp
@@ -3,7 +3,8 @@
 #include <iomanip>
 #include <math.h>
 using namespace std;
-double leibniz(int,int);
+double leibniz(int, int);
+double raphson(double, int,int,int,int);
 int main()
 {
     char resp = 's';
@@ -18,6 +19,26 @@ int main()
         switch (opcion)
         {
         case 1:
+            int num1, num2, num3;
+            cout << "Ingrese los numeros de la ecuacion porfavor: " << endl;
+            cin >> num1;
+            if (num1 <= 0)
+            {
+                cout << "Ingrese un numero mayor a 0: " << endl;
+                cin >> num1;
+            }
+            cout << endl;
+            cin >> num2;
+            cout << endl;
+            cin >> num3;
+            cout << endl;
+            double vertice;
+            vertice = -num2 / (2 * (num1));
+            double ejecucion_1, ejecucion_2;
+            ejecucion_1 = vertice + 200;
+            ejecucion_2 = vertice - 200;
+            cout << "Ejecucion 1 "<<raphson(ejecucion_1,num1,num2,num3,0)<<endl;
+            cout << "Ejecucion 2 "<<raphson(ejecucion_2,num1,num2,num3,0)<<endl;
             break;
         case 2:
         {
@@ -70,7 +91,7 @@ int main()
                     matriz_estandar[i][j] = (matriz[i][j] - media) / (desviacion);
                 }
             }
-            cout<<"Esta es la matriz estandar:"<<endl;
+            cout << "Esta es la matriz estandar:" << endl;
             for (int i = 0; i < tamano; i++)
             {
                 for (int j = 0; j < tamano; j++)
@@ -86,16 +107,31 @@ int main()
             int max_sum;
             cout << "Ingrese el numero maximo de la sumatoria: " << endl;
             cin >> max_sum;
-            cout<<"La respuesta es: "<<leibniz(max_sum,0)*4<<endl;
+            cout << "La respuesta es: " << leibniz(max_sum, 0) * 4 << endl;
             break;
         }
     }
 }
-double leibniz(int max,int cont){
-if(cont==max){
+double leibniz(int max, int cont)
+{
+    if (cont == max)
+    {
+        return 0;
+    }
+    else
+    {
+        return (pow(-1, cont)) / ((2 * cont) + 1) + leibniz(max, cont + 1);
+    }
     return 0;
-}else{
-return (pow(-1,cont))/((2*cont)+1)+leibniz(max,cont+1);
 }
-return 0;
+
+double raphson(double numero, int num1,int num2,int num3,int cont )
+{
+    if(cont==100){
+return numero;
+    }else{
+        double x_subn=raphson(numero,num1,num2,num3,cont+1);
+return x_subn-((num1*pow(x_subn,2))+(num2*x_subn)+num3)/((2*num1*x_subn)+num2);
+    }
+    return 0;
 }
